e73d2d146971357a13ade21732a9e2d563a25537
LinKin-22 <shomnathcse22@gmail.com>
2019-11-09 23:14:28 +0600

    renamed App.js to

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import News from './components/News';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/news" component={News} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
