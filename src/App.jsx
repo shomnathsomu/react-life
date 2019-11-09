ad0f4dc50e559dc1a102f70345316c39c3bd1e35
LinKin-22 <shomnathcse22@gmail.com>
2019-11-09 23:57:06 +0600

    import custom Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import News from './components/News';
+import Navbar from './components/CustomNavbar';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Navbar />
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/news" component={News} />
