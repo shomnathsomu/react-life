d9681e2995483fb8c23c74eafce7c0e5673b9b27
LinKin-22 <shomnathcse22@gmail.com>
2019-11-11 21:06:15 +0600

    Complete home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,29 @@ export default class Home extends Component {
         return(
             <Container>
                 <Jumbotron>
-                    <h2>Welcome to codeLife</h2>
+                    <h2>Welcome to reactJS codeLife</h2>
                     <p>This is how to build a website with React, React-router and React-bootstrap.</p>
+                    <Link to="/about">
+                        <Button bsstyle="primary">Learn More</Button>
+                    </Link>
                 </Jumbotron>
-                <Link to="/about">
-                    <Button bsstyle="primary">About</Button>
-                </Link>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/person-1.jpg" roundedCircle className="profile-pic" />
+                        <h3>Frank</h3>
+                        <p>That's a cooked tree. We'll send him to Washington. These little son of a gun hide in your brush and you just have to push to them out. </p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/person-2.jpg" roundedCircle className="profile-pic" />
+                        <h3>Vanessa</h3>
+                        <p>That's a cooked tree. We'll send him to Washington. These little son of a gun hide in your brush and you just have to push to them out. </p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/person-3.jpg" roundedCircle className="profile-pic" />
+                        <h3>Riff</h3>
+                        <p>That's a cooked tree. We'll send him to Washington. These little son of a gun hide in your brush and you just have to push to them out. </p>
+                    </Col>
+                </Row>
             </Container>
         );
     }
